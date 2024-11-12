e4ab12e8e3cbd2672840e1c72a3a08233848d70b
Selander1998 <selanderantonsson@gmail.com>
2024-11-12 10:17:36 -0500

    implement ejs and mariadb to environment

--- server.js
+++ server.js
@@ -1,97 +1,62 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
+const db = require("./src/scripts/db");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, "src")));
 
-app.get("/", (_, res) => {
-	res.sendFile(path.join(__dirname, "src", "index.html"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+
+const isItemNew = (dateString) => {
+	const itemDate = new Date(dateString);
+	const today = new Date();
+	const dayDifference = (today - itemDate) / (1000 * 60 * 60 * 24);
+	return dayDifference <= 7;
+};
+
+app.get("/", async (_, res) => {
+	try {
+		const [cards] = await db.query("SELECT * FROM cards");
+		cards.forEach((card) => {
+			card.isNew = isItemNew(card.date);
+		});
+		res.render(path.join(__dirname, "src/views", "index.ejs"), { cards });
+	} catch (err) {
+		console.error(err);
+		res.status(500).send("Database Error");
+	}
 });
 
 app.get("/checkout", (_, res) => {
-	res.sendFile(path.join(__dirname, "src", "checkout.html"));
+	res.render(path.join(__dirname, "src/views", "checkout.hejstml"));
 });
 
 app.get("/details", (_, res) => {
-	res.sendFile(path.join(__dirname, "src", "details.html"));
+	res.render(path.join(__dirname, "src/views", "details.ejs"));
 });
 
 app.get("/admin/products/new", (_, res) => {
 	res.sendFile(path.join(__dirname, "src", "/admin/products/new.html"));
 });
 
-app.listen(PORT, () => {
-	console.log(`Server is running on http://192.168.0.6:${PORT}`);
+app.get("/products/:product", (_, res) => {
+	res.sendFile(path.join(__dirname, "src", "/products/details.html"));
 });
 
-app.get("/api/cards", (_, res) => {
-	const cards = [
-		{
-			id: 1,
-			title: "Svart T-Shirt",
-			price: "199kr",
-			brand: "Gant",
-			imageUrl: "https://placehold.co/150x200",
-			date: "2024-10-25",
-		},
-		{
-			id: 2,
-			title: "Vit T-Shirt",
-			price: "199kr",
-			brand: "Gant",
-			imageUrl: "https://placehold.co/150x200",
-			date: "2024-10-26",
-		},
-		{
-			id: 3,
-			title: "Blå T-Shirt",
-			price: "199kr",
-			brand: "Gant",
-			imageUrl: "https://placehold.co/150x200",
-			date: "2024-10-27",
-		},
-		{
-			id: 4,
-			title: "Grön T-Shirt",
-			price: "199kr",
-			brand: "Gant",
-			imageUrl: "https://placehold.co/150x200",
-			date: "2024-10-28",
-		},
-		{
-			id: 5,
-			title: "Lila T-Shirt",
-			price: "199kr",
-			brand: "Gant",
-			imageUrl: "https://placehold.co/150x200",
-			date: "2024-10-29",
-		},
-		{
-			id: 6,
-			title: "Rosa T-Shirt",
-			price: "199kr",
-			brand: "Gant",
-			imageUrl: "https://placehold.co/150x200",
-			date: "2024-10-30",
-		},
-		{
-			id: 7,
-			title: "Orange T-Shirt",
-			price: "199kr",
-			brand: "Gant",
-			imageUrl: "https://placehold.co/150x200",
-			date: "2024-10-31",
-		},
-		{
-			id: 8,
-			title: "Röd T-Shirt",
-			price: "199kr",
-			brand: "Gant",
-			imageUrl: "https://placehold.co/150x200",
-			date: "2024-12-24",
-		},
-	];
-	res.json(cards);
+app.listen(PORT, () => {
+	console.log(`Server is running on http://${process.env.HOST}:${PORT}`);
+});
+
+app.get("/api/products/:product", (req, res) => {
+	const productSlug = req.query.product;
+	const product = cards.find((x) => x.slug === productSlug);
+	if (!product) {
+		return res.status(404).send();
+	}
+	res.send(product);
 });
